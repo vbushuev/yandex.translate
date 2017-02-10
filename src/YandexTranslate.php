d76928eb7e2a3838fded28e1ebaeea9b7a11df78
vbushuev <v.bushuev@gmail.com>
2017-02-11 00:21:24 +0300

    translated

---
+++ src/YandexTranslate.php
@@ -0,0 +1,11 @@
+<?php
+class YandexTranslate{
+    protected $api = [
+        "key"=>"trnsl.1.1.20160808T114104Z.4fca987aa626b8c2.91ed21fc6a7d733075f78f8cca41fcecf4146acd"
+        "host"=>"https://translate.yandex.net/api/v1.5/tr.json/"
+    ];
+    public function translate($a){
+
+    }
+};
+?>
